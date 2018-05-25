cf02a82b9f19e7f94816874dd9e413c912f4bdc2
Matthieu DUVAL <matthieu@duval-dev.fr>
2018-05-25 09:45:03 +0200

    Test du browser_router

--- my-app/src/todo-item/todo-item.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoItem from './todo-item';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TodoItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
